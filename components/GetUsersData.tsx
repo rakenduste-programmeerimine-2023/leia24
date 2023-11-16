d468ef5cd65cf48d45caf2b1165ea9f6efa48edb
Aksel Talvoja <114922105+ajupesu@users.noreply.github.com>
2023-11-16 17:02:27 +0200

    supabase andmebaasi key-de lisamine
    Supabaseis andmebaasi algne disain tehtud, lisatud ka peamised tabelid(võibolla tekib neid juurde). Users tabelisse panin ka esimesed "test" andmed.

---
+++ components/GetUsersData.tsx
@@ -0,0 +1,17 @@
+import { createClient } from '@/utils/supabase/server';
+import { cookies } from 'next/headers';
+
+export default async function Page() {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+
+  // Assuming 'users' is a table in the 'public' schema of the 'Leia24' database
+  const { data: usersData, error } = await supabase.from('users').select();
+
+  if (error) {
+    console.error('Error querying data from users:', error.message);
+    return <div>Error querying data</div>;
+  }
+
+  return <pre>{JSON.stringify(usersData, null, 2)}</pre>;
+}
