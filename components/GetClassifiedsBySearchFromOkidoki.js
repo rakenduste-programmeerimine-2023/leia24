86aeb3ea955b7a53ca7095611ae0fcb1eb9a2e69
aron <114922035+aronpaco@users.noreply.github.com>
2023-11-22 10:52:04 +0200

    bugfix: banner opens correct link

--- components/GetClassifiedsBySearchFromOkidoki.js
+++ components/GetClassifiedsBySearchFromOkidoki.js
@@ -3,9 +3,11 @@ import { JSDOM } from "jsdom";
 import * as React from "react";
 import Typography from "@mui/material/Typography";
 import MenuItem from "@mui/material/MenuItem";
+import { relative } from "path";
 
 // const GetClassifiedsBySearchFromOkidoki = async (search) => {
 const search = "playstation 3";
+let relativeHref;
 const GetClassifiedsBySearchFromOkidoki = async () => {
   console.log(search);
   const res = await fetch(
@@ -18,12 +20,12 @@ const GetClassifiedsBySearchFromOkidoki = async () => {
   const document = dom.window.document;
 
   const links = document.querySelectorAll(".horiz-offer-card__title-link");
-
+  /*
   const classifiedData = Array.from(links).map((link) => {
     const relativeHref = link.getAttribute("href");
     const relativeTitle = link.getAttribute("title");
     return `${relativeTitle}, https://www.okidoki.ee${relativeHref}; `;
-  });
+  });*/
   const hrefs = Array.from(links).map((link) => {
     const relativeHref = link.getAttribute("href");
     return `https://www.okidoki.ee${relativeHref}`;
@@ -36,8 +38,8 @@ const GetClassifiedsBySearchFromOkidoki = async () => {
   return (
     <div>
       <ul>
-        {classifiedData.map((page) => (
-          <a key={page} onClick={console.log("click")} href={hrefs}>
+        {hrefs.map((page) => (
+          <a href={page}>
             <Typography textAlign="center">{page}</Typography>
           </a>
         ))}
