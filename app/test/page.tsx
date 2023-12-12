06099888cef8722db037591740847b5ad840d3d0
aron <114922035+aronpaco@users.noreply.github.com>
2023-12-12 20:06:43 +0200

    refactor: added original data

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -3,6 +3,7 @@
 import * as React from "react";
 import combinedDataOkidoki from "@/components/GetClassifiedsBySearchFromOkidoki";
 import GetClassifiedsBySearch from "@/components/GetClassifiedsBySearch";
+import { Typography } from "@mui/material";
 
 const YourComponent = () => {
   const [data, setData] = React.useState([]);
@@ -18,16 +19,22 @@ const YourComponent = () => {
 
   return (
     <div>
-      <h1>Your Component</h1>
       <ul>
         {data.map((item, index) => (
-          <li key={index}>
-            <p>Title: {item.title}</p>
-            <p>Price: {item.price}</p>
-            <p>Location: {item.location}</p>
-            {/* Add other properties as needed */}
-            <img src={item.imageUrl} alt={item.title} />
-          </li>
+          <div key={index} title={item.title}>
+            <div>
+              <img src={item.imageUrl} alt={item.title} />
+            </div>
+            <Typography textAlign="center">
+              <a href={item.href}>{item.title}</a>
+              <div>
+                Price: {item.price !== undefined ? item.price : "N/A"},
+                Location: {item.location !== undefined ? item.location : "N/A"},
+                Date: {item.date !== undefined ? item.date : "N/A"},{item.site},{" "}
+                <button>Salvesta kuulutus</button>
+              </div>
+            </Typography>
+          </div>
         ))}
       </ul>
     </div>
