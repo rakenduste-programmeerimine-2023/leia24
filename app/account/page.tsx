971fbc179b4e97cf114b8d1b6b3bbe4054168559
Aksel Talvoja <114922105+ajupesu@users.noreply.github.com>
2023-12-13 02:53:23 +0200

    accountpage, login appbaril

---
+++ app/account/page.tsx
@@ -0,0 +1,12 @@
+import AuthButton from "@/components/AuthButton";
+
+export default function YourPage() {
+  
+
+    return (
+      <div>
+        <h1></h1>
+        <AuthButton />
+      </div>
+    );
+  }
